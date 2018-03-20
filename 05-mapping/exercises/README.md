7913fea1d1ac444c6afccf7026b57540a67cfe0f
Erin Brown <erin.brown@miami.edu>
2018-03-20 13:39:49 -0400

    Added 05-mapping.

---
+++ 05-mapping/exercises/README.md
@@ -0,0 +1,31 @@
+# 05 Mapping
+
+This week we're working with SVGs and [Leaflet](http://leafletjs.com/), a library which allows us to map data.
+
+
+## SVGs
+
+Over the next week, find a data set ideal for a choropleth map. Create the data set, collect a count for the data and apply it to the map.
+
+Be sure to add a legend and title to your map (unlike the example in class). You may want to create a data set of colors so they can be applied to both the map and legend.
+
+
+## Leaflet
+
+Using Leaflet, create an interactive map of the college where you obtained your undergraduate degree. Apply the following:
+
+- The correct zoom size/center location. If your campus is massive and stretches beyond your container, make sure the heart is visible to the user.
+
+- Place a marker at the building where you attended most of your classes (the school of your major). In your popup, provide a small blurb about the school and a link to the web site.
+
+- Use polygons (or circles, if applicable) to identify 3-5 of your favorite places on campus. Create a dataset which includes the polygon/circle coordinates, as well as a description of the location (and maybe what was so special about it.) Iterate over this data set to apply the the polygon/circles.
+
+- Consider adjusting the colors used on the map to reflect your school's colors. Are there any other indicators you need to give the users to interact with your map?
+
+## Resources
+
+- [Color Brewer](http://colorbrewer2.org) (choropleth color schemes)
+- [Leaflet](http://leafletjs.com/)
+- [Leaflet: Documentation](http://leafletjs.com/reference-1.3.0.html)
+- [Leaflet: Map Providers](http://leaflet-extras.github.io/leaflet-providers/preview/)
+- [Wikimedia Commons: SVG Maps](https://commons.wikimedia.org/wiki/Category:SVG_maps)
---
+++ 05-mapping/walkthrough/js/teams.js
@@ -0,0 +1,450 @@
+var ncaaTeams = [
+  {
+    team: "Auburn",
+    nickname: "Tigers",
+    city: "Auburn",
+    state: "Alabama",
+    abbr: "al"
+  },
+  {
+    team: "Alabama",
+    nickname: "Crimson Tide",
+    city: "Tuscaloosa",
+    state: "Alabama",
+    abbr: "al"
+  },
+  {
+    team: "Arizona",
+    nickname: "Wildcats",
+    city: "Tucson",
+    state: "Arizona",
+    abbr: "az"
+  },
+  {
+    team: "Arkansas",
+    nickname: "Razorbacks",
+    city: "Fayettville",
+    state: "Arkansas",
+    abbr: "ar"
+  },
+  {
+    team: "San Diego State",
+    nickname: "Aztecs",
+    city: "San Diego",
+    state: "California",
+    abbr: "ca"
+  },
+  {
+    team: "CSU Fullerton",
+    nickname: "Titans",
+    city: "Fullerton",
+    state: "California",
+    abbr: "ca"
+  },
+  {
+    team: "Miami",
+    nickname: "Hurricanes",
+    city: "Coral Gables",
+    state: "Florida",
+    abbr: "fl"
+  },
+  {
+    team: "Florida State",
+    nickname: "Seminoles",
+    city: "Tallahassee",
+    state: "Florida",
+    abbr: "fl"
+  },
+  {
+    team: "Florida",
+    nickname: "Gators",
+    city: "Gainesville",
+    state: "Florida",
+    abbr: "fl"
+  },
+  {
+    team: "Georgia State",
+    nickname: "Panthers",
+    city: "Atlanta",
+    state: "Georgia",
+    abbr: "ga"
+  },
+  {
+    team: "Loyola-Chicago",
+    nickname: "Ramblers",
+    city: "Chicago",
+    state: "Illinois",
+    abbr: "il"
+  },
+  {
+    team: "Purdue",
+    nickname: "Boilermakers",
+    city: "West Lafayette",
+    state: "Indiana",
+    abbr: "in"
+  },
+  {
+    team: "Butler",
+    nickname: "Bulldogs",
+    city: "Indianapolis",
+    state: "Indiana",
+    abbr: "in"
+  },
+  {
+    team: "Kansas State",
+    nickname: "Wildcats",
+    city: "Manhattan",
+    state: "Kansas",
+    abbr: "ks"
+  },
+  {
+    team: "Kansas",
+    nickname: "Jayhawks",
+    city: "Lawrence",
+    state: "Kansas",
+    abbr: "ks"
+  },
+  {
+    team: "Wichita State",
+    nickname: "Shockers",
+    city: "Wichita",
+    state: "Kansas",
+    abbr: "ks"
+  },
+  {
+    team: "Kentucky",
+    nickname: "Wildcats",
+    city: "Lexington",
+    state: "Kentucky",
+    abbr: "ky"
+  },
+  {
+    team: "Murray State",
+    nickname: "Racers",
+    city: "Murray",
+    state: "Kentucky",
+    abbr: "ky"
+  },
+  {
+    team: "UMBC",
+    nickname: "Retrievers",
+    city: "Baltimore",
+    state: "Maryland",
+    abbr: "md"
+  },
+  {
+    team: "Michigan",
+    nickname: "Wolverines",
+    city: "Ann Arbor",
+    state: "Michigan",
+    abbr: "mi"
+  },
+  {
+    team: "Michigan State",
+    nickname: "Spartans",
+    city: "East Lansing",
+    state: "Michigan",
+    abbr: "mi"
+  },
+  {
+    team: "Missouri",
+    nickname: "Tigers",
+    city: "Columbia",
+    state: "Missouri",
+    abbr: "mo"
+  },
+  {
+    team: "Montana",
+    nickname: "Grizzlies",
+    city: "Missoula",
+    state: "Montana",
+    abbr: "mt"
+  },
+  {
+    team: "Creighton",
+    nickname: "Blue Jays",
+    city: "Omaha",
+    state: "Nebraska",
+    abbr: "ne"
+  },
+  {
+    team: "Nevada",
+    nickname: "Wolf Pack",
+    city: "Reno",
+    state: "Nevada",
+    abbr: "nv"
+  },
+  {
+    team: "Seton Hall",
+    nickname: "Pirates",
+    city: "South Orange",
+    state: "New Jersey",
+    abbr: "nj"
+  },
+  {
+    team: "New Mexico State",
+    nickname: "Aggies",
+    city: "Las Cruces",
+    state: "New Mexico",
+    abbr: "nm"
+  },
+  {
+    team: "Buffalo",
+    nickname: "Bulls",
+    city: "Buffalo",
+    state: "New York",
+    abbr: "ny"
+  },
+  {
+    team: "Iona",
+    nickname: "Gaels",
+    city: "New Rochelle",
+    state: "New York",
+    abbr: "ny"
+  },
+  {
+    team: "Syracuse",
+    nickname: "Orange",
+    city: "Syracuse",
+    state: "New York",
+    abbr: "ny"
+  },
+  {
+    team: "St. Bonaventure",
+    nickname: "Bonnies",
+    city: "St. Bonaventure",
+    state: "New York",
+    abbr: "ny"
+  },
+  {
+    team: "Davidson",
+    nickname: "Wildcats",
+    city: "Davidson",
+    state: "North Carolina",
+    abbr: "nc"
+  },
+  {
+    team: "UNC Greensboro",
+    nickname: "Spartans",
+    city: "Greensboro",
+    state: "North Carolina",
+    abbr: "nc"
+  },
+  {
+    team: "North Carolina",
+    nickname: "Tar Heels",
+    city: "Chapel Hill",
+    state: "North Carolina",
+    abbr: "nc"
+  },
+  {
+    team: "Duke",
+    nickname: "Blue Devils",
+    city: "Durham",
+    state: "North Carolina",
+    abbr: "nc"
+  },
+  {
+    team: "North Carolina State",
+    nickname: "Wolfpack",
+    city: "Raleigh",
+    state: "North Carolina",
+    abbr: "nc"
+  },
+  {
+    team: "Wright State",
+    nickname: "Raiders",
+    city: "Dayton",
+    state: "Ohio",
+    abbr: "oh"
+  },
+  {
+    team: "Cincinnati",
+    nickname: "Bearcats",
+    city: "Cincinnati",
+    state: "Ohio",
+    abbr: "oh"
+  },
+  {
+    team: "Xavier",
+    nickname: "Musketeers",
+    city: "Cincinnati",
+    state: "Ohio",
+    abbr: "oh"
+  },
+  {
+    team: "Ohio State",
+    nickname: "Buckeyes",
+    city: "Columbus",
+    state: "Ohio",
+    abbr: "oh"
+  },
+  {
+    team: "Oklahoma",
+    nickname: "Sooners",
+    city: "Norman",
+    state: "Oklahoma",
+    abbr: "ok"
+  },
+  {
+    team: "Bucknell",
+    nickname: "Bison",
+    city: "Lewisburg",
+    state: "Pennsylvania",
+    abbr: "pa"
+  },
+  {
+    team: "Penn",
+    nickname: "Quakers",
+    city: "Philadelphia",
+    state: "Pennsylvania",
+    abbr: "pa"
+  },
+  {
+    team: "Villanova",
+    nickname: "Wildcats",
+    city: "Villanova",
+    state: "Pennsylvania",
+    abbr: "pa"
+  },
+  {
+    team: "Providence",
+    nickname: "Friars",
+    city: "Providence",
+    state: "Rhode Island",
+    abbr: "ri"
+  },
+  {
+    team: "Rhode Island",
+    nickname: "Rams",
+    city: "Kingston",
+    state: "Rhode Island",
+    abbr: "ri"
+  },
+  {
+    team: "Charleston",
+    nickname: "Cougars",
+    city: "Charleston",
+    state: "South Carolina",
+    abbr: "sc"
+  },
+  {
+    team: "Clemson",
+    nickname: "Tigers",
+    city: "Clemson",
+    state: "South Carolina",
+    abbr: "sc"
+  },
+  {
+    team: "South Dakota State",
+    nickname: "Jackrabbits",
+    city: "Brookings",
+    state: "South Dakota",
+    abbr: "sd"
+  },
+  {
+    team: "Tennessee",
+    nickname: "Volunteers",
+    city: "Knoxville",
+    state: "Tennessee",
+    abbr: "tn"
+  },
+  {
+    team: "Lipscomb",
+    nickname: "Bisons",
+    city: "Nashville",
+    state: "Tennessee",
+    abbr: "tn"
+  },
+  {
+    team: "Texas",
+    nickname: "Longhorns",
+    city: "Austin",
+    state: "Texas",
+    abbr: "tx"
+  },
+  {
+    team: "Texas Southern",
+    nickname: "Tigers",
+    city: "Houston",
+    state: "Texas",
+    abbr: "tx"
+  },
+  {
+    team: "Houston",
+    nickname: "Cougars",
+    city: "Houston",
+    state: "Texas",
+    abbr: "tx"
+  },
+  {
+    team: "Texas A&M",
+    nickname: "Aggies",
+    city: "College Station",
+    state: "Texas",
+    abbr: "tx"
+  },
+  {
+    team: "TCU",
+    nickname: "Horned Frogs",
+    city: "Fort Worth",
+    state: "Texas",
+    abbr: "tx"
+  },
+  {
+    team: "Texas Tech",
+    nickname: "Red Raiders",
+    city: "Lubbock",
+    state: "Texas",
+    abbr: "tx"
+  },
+  {
+    team: "Stephen F. Austin",
+    nickname: "Lumberjacks",
+    city: "Nacogdoches",
+    state: "Texas",
+    abbr: "tx"
+  },
+  {
+    team: "Virginia",
+    nickname: "Cavaliers",
+    city: "Charlottesville",
+    state: "Virginia",
+    abbr: "va"
+  },
+  {
+    team: "Virginia Tech",
+    nickname: "Hokies",
+    city: "Blacksburg",
+    state: "Virginia",
+    abbr: "va"
+  },
+  {
+    team: "Radford",
+    nickname: "Highlanders",
+    city: "Radford",
+    state: "Virginia",
+    abbr: "va"
+  },
+  {
+    team: "Gonzaga",
+    nickname: "Bulldogs",
+    city: "Spokane",
+    state: "Washington",
+    abbr: "wa"
+  },
+  {
+    team: "Marshall",
+    nickname: "Thundering Herd",
+    city: "Huntington",
+    state: "West Virginia",
+    abbr: "wv"
+  },
+  {
+    team: "West Virginia",
+    nickname: "Mountaineers",
+    city: "Morgantown",
+    state: "West Virginia",
+    abbr: "wv"
+  }
+]
